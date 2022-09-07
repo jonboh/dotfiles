6555e7a8653bba78c231b4acadb2df07dc378d8c
Jon <jon.bosque.hernando@gmail.com>
2022-09-07 21:14:14 +0200

    update keymap

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,19 +1,18 @@
 local Remap = require("jonbo.keymap")
 local nnoremap = Remap.nnoremap
 
---nnoremap("<C-p>", function()
---    require( telescope_builtin.builtin ).git_files()
---end)
 local telescope_builtin = require( telescope.builtin )
-nnoremap("<leader>f", telescope_builtin.find_files)
 
+-- files
+nnoremap("<leader>tf", telescope_builtin.find_files) -- TODO: dont show hidden files
+-- TODO: grep does not work
 
-nnoremap("<leader>k", telescope_builtin.keymaps)
 -- git
 nnoremap("<leader>gb", telescope_builtin.git_branches)
 nnoremap("<leader>gf", telescope_builtin.git_files)
 
 -- vim related
+nnoremap("<leader>vk", telescope_builtin.keymaps)
 nnoremap("<leader>vb", telescope_builtin.buffers) --view open buffers
 nnoremap("<leader>vh", telescope_builtin.help_tags) -- easily access help with preview
 nnoremap("<leader>vrc", function()
