f99de6b37874909334af0694d4d764bc20b987b5
jonboh <jon.bosque@optimitive.com>
2022-09-27 20:11:46 +0200

    remove already installed plugins

--- .config/nvim/lua/jonbo/packer.lua
+++ .config/nvim/lua/jonbo/packer.lua
@@ -46,25 +46,14 @@ return require( packer ).startup(function(use)
     -- Debugger
     use("mfussenegger/nvim-dap")
     use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
-    use("theHamsta/nvim-dap-virtual-text")
+    use("theHamsta/nvim-dap-virtual-text") -- TODO: configure!
     use("mfussenegger/nvim-dap-python") -- configs for debugpy
 
     -- ThePrimegean List
     --use("sbdchd/neoformat")
---
-    ---- Simple plugins can be specified as strings
-    --use("TimUntersberger/neogit")
---
     ---- TJ created lodash of neovim
-    --use("nvim-lua/plenary.nvim")
     --use("nvim-lua/popup.nvim")
-    --use("nvim-telescope/telescope.nvim")
---
     ---- All the things
-    --use("neovim/nvim-lspconfig")
-    --use("hrsh7th/cmp-nvim-lsp")
-    --use("hrsh7th/cmp-buffer")
-    --use("hrsh7th/nvim-cmp")
     --use("tzachar/cmp-tabnine", { run = "./install.sh" })
     --use("onsails/lspkind-nvim")
     --use("nvim-lua/lsp_extensions.nvim")
@@ -72,15 +61,8 @@ return require( packer ).startup(function(use)
     --use("simrat39/symbols-outline.nvim")
     --use("L3MON4D3/LuaSnip")
     --use("saadparwaiz1/cmp_luasnip")
---
     ---- Primeagen doesn"t create lodash
-    --use("ThePrimeagen/git-worktree.nvim")
     --use("ThePrimeagen/harpoon")
---
-
     --use("nvim-treesitter/playground")
 
-    --use("mfussenegger/nvim-dap")
-    --use("rcarriga/nvim-dap-ui")
-    --use("theHamsta/nvim-dap-virtual-text")
 end)
