d9dc736d919d415bc258ec6af7fdad3bf0b9d0aa
jonboh <jon.bosque@optimitive.com>
2022-09-29 10:58:04 +0200

    use named functions so that their name appears on telescopes keymap menu

--- .config/nvim/after/plugin/keymap/telescope.lua
+++ .config/nvim/after/plugin/keymap/telescope.lua
@@ -5,16 +5,15 @@ local telescope_builtin = require( telescope.builtin )
 --require("telescope").load_extension("git_worktree") -- not yet working
 
 -- files
-nnoremap("<leader>tf", telescope_builtin.find_files) -- TODO: dont show hidden files
-nnoremap("<leader>tg", function()
-    telescope_builtin.live_grep(
---    { search = vim.fn.input("Grep For > ")}
-)
-end)
-
-nnoremap("<leader>tw", function()
+live_grep = function()
+    telescope_builtin.live_grep()--{ search = vim.fn.input("Grep For > ")}
+end
+grep_word = function()
     telescope_builtin.grep_string { search = vim.fn.expand("<cword>") }
-end)
+end
+nnoremap("<leader>tf", telescope_builtin.find_files) -- TODO: dont show hidden files
+nnoremap("<leader>tg", live_grep)
+nnoremap("<leader>tw", grep_word)
 
 -- git
 nnoremap("<leader>gb", telescope_builtin.git_branches)
@@ -26,10 +25,11 @@ nnoremap("<leader>gw", require( telescope ).extensions.git_worktree.git_worktree
 -- <c-f> - toggles forcing of the next deletion
 
 -- vim related
+search_dotfiles = function()
+    require( jonbo.telescope ).search_dotfiles({ hidden = true }) -- config files
+end
 nnoremap("<leader>vk", telescope_builtin.keymaps)
 nnoremap("<leader>vb", telescope_builtin.buffers) --view open buffers
 nnoremap("<leader>vh", telescope_builtin.help_tags) -- easily access help with preview
-nnoremap("<leader>vrc", function()
-    require( jonbo.telescope ).search_dotfiles({ hidden = true }) -- config files
-end)
+nnoremap("<leader>vrc", search_dotfiles)
 
