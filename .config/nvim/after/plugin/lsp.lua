d59cf2c6bab14fefd24bdbd7e465b61d74bee1cd
jonboh <jon.bosque@optimitive.com>
2022-09-30 12:57:12 +0200

    small changes in keymap lsp

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -34,10 +34,6 @@ cmp.setup({
       { name =  buffer  },
     })
   })
-nnoremap( [d , vim.diagnostic.goto_prev)
-nnoremap( ]d , vim.diagnostic.goto_next)
-nnoremap( <leader>o , "<cmd>:ClangdSwitchSourceHeader<CR>")
--- nnoremap("<leader>vd", vim.diagnostic.open_float)
 -- vim.keymap.set( n ,  <space>e , vim.diagnostic.open_float, opts)
 
 -- Use an on_attach function to only map the following keys
@@ -56,34 +52,14 @@ local on_attach = function(client, bufnr)
   vim.keymap.set( n ,  <leader>rn , vim.lsp.buf.rename, bufopts)
   vim.keymap.set( n ,  <leader>f , vim.lsp.buf.formatting, bufopts)
   vim.keymap.set( n ,  <leader>ca , vim.lsp.buf.code_action, bufopts)
-    nnoremap("<leader>vd", function() vim.diagnostic.open_float() end)
-    nnoremap("[d", function() vim.diagnostic.goto_next() end)
-    nnoremap("]d", function() vim.diagnostic.goto_prev() end)
+  vim.keymap.set( n , "<leader>vd", vim.diagnostic.open_float)
+  vim.keymap.set( n , "[d", vim.diagnostic.goto_next)
+  vim.keymap.set( n , "]d", vim.diagnostic.goto_prev)
 end
---[[
-function()
---			nnoremap("<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
---			nnoremap("<leader>vca", function() vim.lsp.buf.code_action() end)
---			nnoremap("<leader>vco", function() vim.lsp.buf.code_action({
- --               filter = function(code_action)
-  --                  if not code_action or not code_action.data then
-   --                     return false
-    --                end
-
-  --                  local data = code_action.data.id
- --                   return string.sub(data, #data - 1, #data) == ":0"
-   --             end,
-     --           apply = true
-    --        }) end)
-	--		nnoremap("<leader>vrn", function() vim.lsp.buf.rename() end)
-			inoremap("<C-h>", function() vim.lsp.buf.signature_help() end)
-		end
---]]
-
-
 
 -- LSP Configuration
 -- C++ 
+nnoremap( <leader>o , "<cmd>:ClangdSwitchSourceHeader<CR>") -- TODO: add this on attach
 require lspconfig .clangd.setup {
 		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()), -- advertise capabilities
 		cmd = { "clangd-12", "--background-index" },
