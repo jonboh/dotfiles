b2329d26743b2b4e29258e2d7ab0b925f21a50ed
Jon <jon.bosque.hernando@gmail.com>
2022-10-05 21:21:25 +0200

    remove things i wont use

--- .config/nvim/lua/jonbo/packer.lua
+++ .config/nvim/lua/jonbo/packer.lua
@@ -75,15 +75,9 @@ return require( packer ).startup(function(use)
     ---- TJ created lodash of neovim
     --use("nvim-lua/popup.nvim")
     ---- All the things
-    --use("tzachar/cmp-tabnine", { run = "./install.sh" })
     --use("onsails/lspkind-nvim")
-    --use("nvim-lua/lsp_extensions.nvim")
     --use("glepnir/lspsaga.nvim")
-    --use("simrat39/symbols-outline.nvim")
     --use("L3MON4D3/LuaSnip")
     --use("saadparwaiz1/cmp_luasnip")
-    ---- Primeagen doesn"t create lodash
-    --use("ThePrimeagen/harpoon")
-    --use("nvim-treesitter/playground")
 
 end)
