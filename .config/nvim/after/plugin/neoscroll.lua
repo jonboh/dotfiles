1d370c0709948380137c907b9f3c76b7f64ad96c
jonboh <jon.bosque@optimitive.com>
2022-10-11 10:29:23 +0200

    fix neoscroll overriding harpoon c-e

--- .config/nvim/after/plugin/neoscroll.lua
+++ .config/nvim/after/plugin/neoscroll.lua
@@ -1,15 +1,26 @@
-require( neoscroll ).setup({
-    -- Set any options as needed
+ require( neoscroll ).setup({
+    -- All these keys will be mapped to their corresponding default scrolling animation
+    mappings = {},
+    hide_cursor = true,          -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+    easing_function = nil,       -- Default easing function
+    pre_hook = nil,              -- Function to run before the scrolling animation starts
+    post_hook = nil,             -- Function to run after the scrolling animation ends
+    performance_mode = false,    -- Disable "Performance Mode" on all buffers.
 })
-
+--
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
 t[ <C-u> ] = { scroll , { -vim.wo.scroll ,  true ,  35 }}
 t[ <C-d> ] = { scroll , {  vim.wo.scroll ,  true ,  35 }}
 t[ <C-b> ] = { scroll , { -vim.api.nvim_win_get_height(0) ,  true ,  45 }}
 t[ <C-f> ] = { scroll , {  vim.api.nvim_win_get_height(0) ,  true ,  45 }}
-t[ <C-y> ] = { scroll , { -0.10 ,  false ,  10 }}
-t[ <C-e> ] = { scroll , {  0.10 ,  false ,  10 }}
+t[ <C-k> ] = { scroll , { -0.10 ,  false ,  10 }}
+t[ <C-j> ] = { scroll , {  0.10 ,  false ,  10 }}
+t[ <C-Down> ] = { scroll , { -0.10 ,  false ,  10 }}
+t[ <C-Up> ] = { scroll , { 0.10 ,  false ,  10 }}
 t[ zt ]    = { zt , { 25 }}
 t[ zz ]    = { zz , { 25 }}
 t[ zb ]    = { zb , { 25 }}
