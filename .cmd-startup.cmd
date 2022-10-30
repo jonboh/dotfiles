31446b60101a885245076ef3742b13dcee083015
jonboh <jon.bosque.hernando@gmail.com>
2022-10-30 12:02:26 +0100

    remove windows things

--- .cmd-startup.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-doskey dotfiles=git --git-dir=%userprofile%/dotfiles --work-tree=%userprofile% $*
-doskey gs=git status $*
-doskey ls=exa -1 --group-directories-first $*
-doskey l=exa --icons -F -1 --group-directories-first $*
-doskey la=exa --icons -F -1 --group-directories-first -a $*
-doskey ll=exa --icons -F -1 --group-directories-first -l -a $*
--- scripts/autotrader_main.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set autotrader_location=C:/dev/autotrader
-set autotrader_conda_env=py38
-set back_pwd=%cd%
-
-cd /D %autotrader_location%
-call conda activate %autotrader_conda_env%
-call python main.py
-cd /D %back_pwd%
