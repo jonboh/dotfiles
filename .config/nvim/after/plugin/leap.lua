430bb7f1b30d23567e6e226673afad1d2714f620
jonboh <jon.bosque.hernando@gmail.com>
2022-11-19 12:35:37 +0100

    modify leap navigation label keys

--- .config/nvim/after/plugin/leap.lua
+++ .config/nvim/after/plugin/leap.lua
@@ -1,1 +1,20 @@
-require( leap ).set_default_keymaps() -- TODO: use only lowercase dvorak homerow?
+require( leap ).set_default_keymaps()
+require( leap ).setup({
+max_phase_one_targets = nil,
+highlight_unlabeled_phase_one_targets = false,
+max_highlighted_traversal_targets = 10,
+case_sensitive = false,
+equivalence_classes = {   \t\r\n , },
+substitute_chars = {},
+labels = {  a ,  o ,  e ,  u ,  i ,  d ,  h ,  t ,  n ,  s ,  A ,  O ,  E ,  U ,  I ,  D ,  H ,  T ,  N ,  S },
+safe_labels = {  a ,  o ,  e ,  u ,  h ,  t ,  n ,  s ,  A ,  O ,  E ,  U ,  I ,  H ,  T ,  N ,  S },
+special_keys = {
+  repeat_search =  <enter> ,
+  next_phase_one_target =  <enter> ,
+  next_target = { <enter> ,  ; },
+  prev_target = { <tab> ,  , },
+  next_group =  <space> ,
+  prev_group =  <tab> ,
+  multi_accept =  <enter> ,
+  multi_revert =  <backspace> ,
+}})
