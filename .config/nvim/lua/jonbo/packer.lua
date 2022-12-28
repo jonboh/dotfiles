f0205aaf6754e8c3183d7fb69eeca363e2fd30f6
jonboh <jon.bosque.hernando@gmail.com>
2022-12-28 16:21:53 +0100

    move lsp configuration to lsp-zero

--- .config/nvim/lua/jonbo/packer.lua
+++ .config/nvim/lua/jonbo/packer.lua
@@ -97,10 +97,27 @@ return require( packer ).startup(function(use)
     use("nvim-treesitter/nvim-treesitter-context") -- for the sticky context lines
 
     -- LSP
-    use( neovim/nvim-lspconfig ) -- Configurations for Nvim LSP
-    use("hrsh7th/nvim-cmp") -- completion engine
-    use("hrsh7th/cmp-buffer") -- source for in buffer completions
-    use("hrsh7th/cmp-nvim-lsp") -- source for builtin nvim lsp client
+    use {
+       VonHeikemen/lsp-zero.nvim ,
+      requires = {
+        -- LSP Support
+        { neovim/nvim-lspconfig },
+        { williamboman/mason.nvim },
+        { williamboman/mason-lspconfig.nvim },
+
+        -- Autocompletion
+        { hrsh7th/nvim-cmp },
+        { hrsh7th/cmp-buffer },
+        { hrsh7th/cmp-path },
+        { saadparwaiz1/cmp_luasnip },
+        { hrsh7th/cmp-nvim-lsp },
+        { hrsh7th/cmp-nvim-lua },
+
+        -- Snippets
+        { L3MON4D3/LuaSnip },
+        { rafamadriz/friendly-snippets },
+      }
+    }
 
     -- Debugger
     use("mfussenegger/nvim-dap")
