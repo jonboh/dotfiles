c7b51669f8434feb42af45a4eda780015466dc84
jonboh <jon.bosque.hernando@gmail.com>
2023-01-10 18:44:24 +0100

    setup obsidan formatting

--- .config/nvim/after/plugin/obsidian.lua
+++ .config/nvim/after/plugin/obsidian.lua
@@ -1,13 +1,27 @@
 require("obsidian").setup({
     dir = "~/doc/obsidian",
-    notes_subdir = "Zettlekasten",
     daily_notes = {
         folder = "Journal",
     },
     completion = {
         nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
         use_advanced_uri = true,
-    }
+    },
+    note_id_func = function(title)
+        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+        local suffix = ""
+        if title ~= nil then
+          -- If title is given, transform it into valid file name.
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          -- If title is nil, just add 4 random uppercase letters to the suffix.
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return suffix .. "-" .. tostring(os.time())
+      end
+
 })
 
 -- TODO: check difference with https://github.com/epwalsh/obsidian.nvim#using-nvim-treesitter
