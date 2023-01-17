92d77cae93886fc887b7386455304c85e4b8e1c5
jonboh <jon.bosque.hernando@gmail.com>
2023-01-17 17:38:46 +0100

    auto-install plugins

--- .config/nvim/lua/jonbo/packer.lua
+++ .config/nvim/lua/jonbo/packer.lua
@@ -1,7 +1,15 @@
--- This file can be loaded by calling `lua require( plugins )` from your init.vim
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath( data ).. /site/pack/packer/start/packer.nvim 
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ git ,  clone ,  --depth ,  1 ,  https://github.com/wbthomason/packer.nvim , install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
 
--- Only required if you have packer configured as `opt`
--- vim.cmd [[packadd packer.nvim]]
+local packer_bootstrap = ensure_packer()
 
 return require( packer ).startup(function(use)
     -- Packer can manage itself
@@ -145,6 +153,10 @@ return require( packer ).startup(function(use)
     }
     -- Games
     use  ThePrimeagen/vim-be-good 
+
+    if packer_bootstrap then -- Automatically set up configuration after clonning packer.nvim
+        require( packer ).sync()
+    end
 
     -- TODO: setup github integration with https://github.com/pwntester/octo.nvim
         -- be able to review PRs from nvim woah! :D
