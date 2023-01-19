3809c09cc8c5995c1a5120c68e908d0b8918fa84
jonboh <jon.bosque.hernando@gmail.com>
2023-01-19 09:39:58 +0100

    add scratchpad for i3 and updateall for vault

---
+++ .config/i3/scratchpad.sh
@@ -0,0 +1,8 @@
+scratchpad_cmd= alacritty --class Dropdown -e zsh 
+
+running_cmd=$(ps aux | grep "${scratchpad_cmd}" | wc --lines)
+# running_cmd will be 1 when no running scratchpad is running, because
+# grep will match itself on ps aux
+if [ "${running_cmd}" == "1" ]; then
+    $scratchpad_cmd
+fi
