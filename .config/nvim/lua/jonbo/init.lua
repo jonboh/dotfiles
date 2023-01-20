39f9b95d89bf8819e4bade673442d20aa5c064fa
jonboh <jon.bosque.hernando@gmail.com>
2023-01-20 23:44:17 +0100

    add easy print

--- .config/nvim/lua/jonbo/init.lua
+++ .config/nvim/lua/jonbo/init.lua
@@ -2,3 +2,8 @@ require("jonbo.set")
 require("jonbo.packer")
 require("jonbo.debugger")
 require("jonbo.telescope")
+
+P = function(v) -- tj easy printing
+    print(vim.inspect(v))
+    return v
+end
