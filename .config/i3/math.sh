170c514ba912bb3fb5c7e89ad066169831bd9590
jonboh <jon.bosque.hernando@gmail.com>
2023-01-20 23:43:53 +0100

    fix missing change

--- .config/i3/math.sh
+++ .config/i3/math.sh
@@ -1,4 +1,4 @@
-scratchpad_cmd= alacritty --class ScratchpadMath -e zsh 
+scratchpad_cmd= alacritty --class ScratchpadMath -e python 
 
 running_cmd=$(ps aux | grep "${scratchpad_cmd}" | wc --lines)
 # running_cmd will be 1 when no running scratchpad is running, because
