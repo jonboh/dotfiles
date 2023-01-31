eea270abb8449ad85d82111f8ae8574328b32102
jonboh <jon.bosque.hernando@gmail.com>
2023-01-31 10:50:31 +0100

    add tsplayground

--- .config/nvim/lua/jonbo/packer.lua
+++ .config/nvim/lua/jonbo/packer.lua
@@ -98,6 +98,7 @@ return require( packer ).startup(function(use)
     use("nvim-treesitter/nvim-treesitter", {run = ":TSUpdate"})
     use( nvim-treesitter/nvim-treesitter-refactor ) -- for symbol under cursor highlighting
     use("nvim-treesitter/nvim-treesitter-context") -- for the sticky context lines
+    use( nvim-treesitter/playground )
 
     -- LSP
     use {
