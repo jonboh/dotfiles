448c3c657dc5cd48c1488354bb145e86a2a9a987
jonboh <jon.bosque.hernando@gmail.com>
2023-04-12 11:01:28 +0200

    fix indent-blankline coloring

--- .config/nvim/after/plugin/indent-blankline.lua
+++ .config/nvim/after/plugin/indent-blankline.lua
@@ -1,12 +1,23 @@
 vim.opt.list = true
---vim.opt.listchars:append "space:⋅" -- weird blue coloring on indent points
+vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↴"
 
+-- vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+-- vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+vim.cmd([[highlight IndentBlanklineContextChar guifg=#af00af gui=nocombine]])
+vim.cmd([[highlight IndentBlanklineContextStart guisp=#af00af gui=underline]])
 require("indent_blankline").setup {
-    char = "▎",
-    space_char_blankline = " ",
-    show_current_context = false, -- current context not working correctly
-    show_current_context_start = false, -- underline not correctly colored on windows terminal
-    use_treesitter_scope = false,
+    char="▎"   ,
+    space_char_blankline = "",
+    show_current_context = true,
+    show_current_context_start = true,
+    show_trailing_blankline_indent = false,
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
 }
--- TODO: Fix coloring
