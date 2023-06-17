7c9e232ff45cb9f04417d17ae7a94c9245ab1298
jonboh <jon.bosque.hernando@gmail.com>
2023-06-17 12:35:41 +0200

    add gdb and rr configuration

--- .config/nvim/lua/jonbo/debugger/cpp.lua
+++ .config/nvim/lua/jonbo/debugger/cpp.lua
@@ -1,32 +1,81 @@
 local dap = require( dap )
 
-dap.adapters.codelldb = {
-  type =  server ,
-  port = "${port}",
-  executable = {
-    -- CHANGE THIS to your path!
-    command =  codelldb ,
-    args = {"--port", "${port}"},
 
-    -- On windows you may have to uncomment this:
-    -- detached = false,
-  }
+local codelldb_extension_path = vim.fn.stdpath("data").."/mason".."/packages".."/codelldb".."/extension"
+local codelldb_path = codelldb_extension_path..  /adapter/codelldb 
+local cpptools_extension_path = vim.fn.stdpath("data").."/mason".."/packages".."/cpptools".."/extension"
+local cpptools_path = cpptools_extension_path.."/debugAdapters/bin/OpenDebugAD7"
+dap.adapters.codelldb = {
+    type =  server ,
+    port = "${port}",
+    executable = {
+        command = codelldb_path,
+        args = {"--port", "${port}"},
+
+        -- On windows you may have to uncomment this:
+        -- detached = false,
+    }
 }
+
+dap.adapters.cppdbg = {
+    id =  cppdbg ,
+    type =  executable ,
+    command = cpptools_path,
+}
+
 
 dap.configurations.cpp = {
-  {
-    name = "Launch file",
-    type = "codelldb",
-    request = "launch",
-    program = function()
-      return vim.fn.input( Path to executable:  , vim.fn.getcwd() ..  / ,  file )
-    end,
-    cwd = function()
-      return vim.fn.input( Path to cwd:  , vim.fn.getcwd() ..  / ,  file )
-    end,
-    stopOnEntry = false,
-  },
+    {
+        name = "(lldb) Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input( Path to executable:  , vim.fn.getcwd() ..  / ,  file )
+        end,
+        cwd =  ${workspaceFolder} ,
+        stopOnEntry = true,
+    },
+    {
+        name = "(gdb) Launch file",
+        type = "cppdbg",
+        request = "launch",
+        program = function()
+            return vim.fn.input( Path to executable:  , vim.fn.getcwd() ..  / ,  file )
+        end,
+        cwd =  ${workspaceFolder} ,
+        stopAtEntry = true,
+    },
+    {
+        name= "rr",
+        type= "cppdbg",
+        request= "launch",
+        program= "${workspaceRoot}/../obj-ff-dbg/dist/bin/firefox",
+        args= {},
+        miDebuggerServerAddress= "localhost:50505",
+        stopAtEntry= false,
+        cwd= "${workspaceRoot}/../obj-ff-dbg/dist/bin",
+        environment= {},
+        externalConsole= true,
+        linux= {
+            MIMode= "gdb",
+            setupCommands= {
+                {
+                    description= "Setup to resolve symbols",
+                    text= "set sysroot /",
+                    ignoreFailures= false
+                },
+            },
+        },
+        osx= {
+            MIMode= "gdb"
+        },
+        windows= {
+            MIMode= "gdb"
+        }
+    }
 }
+
+
 -- TODO: fix cwd management
 -- If you want to use this for Rust and C, add something like this:
 
