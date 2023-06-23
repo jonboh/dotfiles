ff826600f1fd0d425f29dc780cd7ae5060071277
jonboh <jon.bosque.hernando@gmail.com>
2023-06-23 09:35:22 +0200

    update harpoon to mstrdy

--- .config/nvim/after/plugin/harpoon.lua
+++ .config/nvim/after/plugin/harpoon.lua
@@ -2,11 +2,11 @@ local nnoremap = require("jonbo.keymap").nnoremap
 
 local silent = { silent = true }
 
-nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
-nnoremap("<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
+nnoremap("<leader>h", function() require("harpoon.mark").add_file() end, silent)
+nnoremap("<C-h>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
 
-nnoremap("<C-h>", function() require("harpoon.ui").nav_file(1) end, silent)
-nnoremap("<C-t>", function() require("harpoon.ui").nav_file(2) end, silent)
-nnoremap("<C-n>", function() require("harpoon.ui").nav_file(3) end, silent)
-nnoremap("<C-s>", function() require("harpoon.ui").nav_file(4) end, silent)
+nnoremap("<C-n>", function() require("harpoon.ui").nav_file(1) end, silent)
+nnoremap("<C-e>", function() require("harpoon.ui").nav_file(2) end, silent)
+nnoremap("<C-a>", function() require("harpoon.ui").nav_file(3) end, silent)
+nnoremap("<C-i>", function() require("harpoon.ui").nav_file(4) end, silent)
 
