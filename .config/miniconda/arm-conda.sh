6b33cf20fabd9f309d42d082849faf074cdba922
jonboh <jon.bosque.hernando@gmail.com>
2023-06-30 20:12:22 +0200

    prevent notification to taking too much horizontal space

--- .config/miniconda/arm-conda.sh
+++ .config/miniconda/arm-conda.sh
@@ -1,2 +1,3 @@
 # source this file to make conda available on your shell instance
-eval "$(/home/jon/miniconda3/bin/conda shell.zsh hook)"
+# eval "$(/home/jon/miniconda3/bin/conda shell.zsh hook)"
+eval "$(/opt/miniconda3/bin/conda shell.zsh hook)"
