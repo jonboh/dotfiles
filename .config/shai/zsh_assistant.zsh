eb936d983e3ec2956f9d21097203cd21246f4ac2
jonboh <jon.bosque.hernando@gmail.com>
2023-08-22 09:03:10 +0200

    add shai

---
+++ .config/shai/zsh_assistant.zsh
@@ -0,0 +1,16 @@
+os="Linux" # use your distro to get more specific instructions
+shell="zsh"
+model="open-aigpt35-turbo"
+
+ask-assistant() {
+    VISUAL="shai ask --operating-system \"$os\" --shell \"$shell\" --model $model --edit-file" zle edit-command-line
+}
+explain-assistant() {
+    VISUAL="shai explain --operating-system \"$os\" --shell \"$shell\" --model $model --edit-file" zle edit-command-line
+}
+# Bind a key combination to trigger the custom widget
+zle -N ask-assistant
+zle -N explain-assistant
+bindkey  ^[s  ask-assistant
+bindkey  ^[e  explain-assistant
+
